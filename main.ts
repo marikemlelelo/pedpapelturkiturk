5f8704903a9bd01d0226a2c789458ef79bf81ced
marikemlelelo <165197146+marikemlelelo@users.noreply.github.com>
2024-04-12 13:30:46 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+let x = 0
+input.onGesture(Gesture.Shake, function () {
+    x = randint(0, 2)
+    if (x == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (x == 1) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
+basic.forever(function () {
+	
+})
